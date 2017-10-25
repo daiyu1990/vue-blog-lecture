5dfc13b7538848920fcf98c1d07c01c8470c29cd
KevinFan <kevin_ypfan@yahoo.com.tw>
2017-10-25 18:11:37 +0800

    little fixed

--- src/components/post/CreatePost.vue
+++ src/components/post/CreatePost.vue
@@ -14,7 +14,11 @@
         <label for="description">Description</label>
         <textarea class="form-control" id="description" rows="3" v-model="description" wrap="virtual" required></textarea>
       </div>
-      <button type="submit" class="btn btn-primary">Create</button>
+      <!-- <button type="submit" class="btn btn-primary">Create</button> -->
+      <button type="submit" class="btn btn-primary" :disabled="loading">
+        <span v-if="!loading">Create</span>
+        <span v-if="loading"><i class='fa fa-spinner fa-spin'></i> Lodding</span>
+      </button>
     </form>
     <div class="alert alert-danger" role="alert" v-if="status">
       {{status}}
@@ -29,16 +33,20 @@ export default {
       title: '',
       subtopic: '',
       description: '',
-      status: ''
+      status: '',
+      loading: false
     }
   },
   methods: {
     onSubmit () {
+      this.loading = true;
       this.axios.post('https://nuu-leture-blog.herokuapp.com/api/posts', {title: this.title, subtopic: this.subtopic, description: this.description, time: this.$moment().valueOf()}, { headers: {token: window.localStorage.getItem('token')} })
         .then((response) => {
           this.$store.dispatch('getPosts')
+          this.loading = false;
           this.$router.push('/')
         }).catch((err) => {
+          this.loading = false;
           this.status = err.response.data
         })
     }
